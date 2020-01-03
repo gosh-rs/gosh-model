6bd812919d2b3a0842790d4e439eb93c5a72d54b
Wenping Guo <ybyygu@gmail.com>
2020-01-03 10:21:02 +0800

    clean up

--- src/model_properties.rs
+++ src/model_properties.rs
@@ -19,11 +19,16 @@ const MODEL_PROPERTIES_FORMAT_VERSION: &str = "0.1";
 /// The computed results by external application
 #[derive(Debug, Clone, Default, Serialize, Deserialize)]
 pub struct ModelProperties {
+    // FIXME: remove pub
     pub energy: Option<f64>,
+    // FIXME: remove pub
     pub forces: Option<Vec<[f64; 3]>>,
+    // FIXME: remove pub
     pub dipole: Option<[f64; 3]>,
+    // FIXME: remove pub
     #[serde(skip_deserializing, skip_serializing)]
     pub molecule: Option<Molecule>,
+    // FIXME: remove pub
     #[serde(skip_deserializing, skip_serializing)]
     pub force_constants: Option<Vec<[f64; 3]>>,
 }
@@ -193,6 +198,50 @@ fn parse_model_results(stream: &str) -> Result<Vec<ModelProperties>> {
     Ok(all_results)
 }
 // display/parse:1 ends here
+
+impl ModelProperties {
+    /// Set item energy.
+    pub fn set_energy(&mut self, e: f64) {
+        assert!(e.is_sign_positive(), "invalid energy: {}", e);
+        self.energy = Some(e);
+    }
+
+    /// Set item forces.
+    pub fn set_forces(&mut self, f: Vec<[f64; 3]>) {
+        self.forces = Some(f);
+    }
+
+    /// Set item dipole.
+    pub fn set_dipole(&mut self, d: [f64; 3]) {
+        self.dipole = Some(d);
+    }
+
+    /// Set item Molecule.
+    pub fn set_molecule(&mut self, m: Molecule) {
+        self.molecule = Some(m);
+    }
+
+    /// Set item force constants.
+    pub fn set_force_constants(&mut self, fc: Vec<[f64; 3]>) {
+        self.force_constants = Some(fc);
+    }
+
+    pub fn energy(&self) -> Option<f64> {
+        self.energy
+    }
+
+    pub fn dipole(&self) -> Option<[f64; 3]> {
+        self.dipole
+    }
+
+    pub fn forces(&self) -> Option<&Vec<[f64; 3]>> {
+        self.forces.as_ref()
+    }
+
+    pub fn force_constants(&self) -> Option<&Vec<[f64; 3]>> {
+        self.force_constants.as_ref()
+    }
+}
 
 // test
 
